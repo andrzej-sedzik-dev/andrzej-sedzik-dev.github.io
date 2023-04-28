e9172ab6d26d4785d21eecedfe19b7c6d01c0a5c
Andrzej Sędzik <39155518+andrzej-sedzik-dev@users.noreply.github.com>
2023-04-28 20:26:42 +0200

    fix uri 10

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     // const apiRoot = 'https://tasks-prod-kodilla-tasks-k6sear.mo1.mogenius.io/v1/tasks';
     // const trelloApiRoot = 'https://tasks-prod-kodilla-tasks-k6sear.mo1.mogenius.io/v1/tasks/v1/trello';
 
-    const apiRoot = 'http://localhost:8080/v1/tasks/createTask';
+    const apiRoot = 'http://localhost:8080/v1/tasks';
     const trelloApiRoot = 'http://localhost:8080/v1/trello';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
@@ -68,7 +68,7 @@
     }
 
     function getAllTasks() {
-      const requestUrl = apiRoot;
+      const requestUrl = apiRoot/createTask;
 
       $.ajax({
         url: requestUrl,
